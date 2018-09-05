647f69a9d808ca3795abe2ff9902438f4f47d93f
blue <bububai@sina.com>
2018-09-05 16:32:49 +0800

    Update map.c
    add trunk method & change set method can replace value

--- map.c
+++ map.c
@@ -1,41 +1,40 @@
 #include "map.h"
-#include <stdio.h>
+#include "log.h"
 #include <stdlib.h>
 #include <stdbool.h>
-#include <signal.h>
 
 struct map_t map_allocate()
 {
 	struct map_t map;
 	map.cell_count = 1024;
-	map.cells = malloc(sizeof(struct cell_t) * map.cell_count);
+	map.cells = (cell_t*)malloc(sizeof(struct cell_t) * map.cell_count);
 	for (int i = 0; i<map.cell_count; i++)
 	{
 		map.cells[i].used = 0;
 		map.cells[i].size = 1;
-		map.cells[i].keys = malloc(sizeof(int) * map.cells[i].size);
-		map.cells[i].values = malloc(sizeof(int) * map.cells[i].size);
+		map.cells[i].keys = (int*)malloc(sizeof(int) * map.cells[i].size);
+		map.cells[i].values = (int*)malloc(sizeof(int) * map.cells[i].size);
 	}
 	return map;
 }
 
-bool map_set(struct map_t* map , int key , int value)
-{
-	int index = key % map->cell_count;
-	
-	if(map->cells[index].used == 0)
-	{
-		map->cells[index].keys[0] = key;
-		map->cells[index].values[0] = value;
-		map->cells[index].used++;
-		return true;
-	}
-	
-	for (int i=0; i < map->cells[index].used; i++)
-		if (map->cells[index].keys[i] == key)
-			return false; // duplicate key found
-	
-	
+bool map_set(struct map_t* map , int key , int value) {
+    int index = key % map->cell_count;
+
+    if (map->cells[index].used == 0) {
+        map->cells[index].keys[0] = key;
+        map->cells[index].values[0] = value;
+        map->cells[index].used++;
+        return true;
+    }
+
+    for (int i = 0; i < map->cells[index].used; i++) {
+        if (map->cells[index].keys[i] == key) {
+            map->cells[index].values[i] = value;
+            return true;
+        }
+    }
+
 	if(map->cells[index].used == map->cells[index].size)
 	{
 		map->cells[index].size *= 3;
@@ -46,6 +45,7 @@ bool map_set(struct map_t* map , int key , int value)
 	map->cells[index].keys[map->cells[index].used] = key;
 	map->cells[index].values[map->cells[index].used] = value;
 	map->cells[index].used++;
+
 	return true;
 }
 
@@ -64,17 +64,21 @@ bool map_contains_key(struct map_t* map , int key)
 }
 
 // does not check if key exists
-int map_get(struct map_t* map , int key)
+int map_get(struct map_t* map, int key)
 {
 	int index = key % map->cell_count;
 	
 	for (int i=0; i < map->cells[index].used; i++)
 		if(map->cells[index].keys[i] == key)
 			return map->cells[index].values[i];
-	
+
+	return 0;
+
+	/*
 	fprintf(stderr , "no key found! detected unsafe usage , check if key exists first");
 	exit(EXIT_FAILURE);
 	return -1;
+	 */
 }
 
 int map_delete(struct map_t* map , int key)
@@ -103,4 +107,11 @@ int map_delete(struct map_t* map , int key)
 	
 	map->cells[index].used--;
 	return true;
+}
+
+void map_trunk(struct map_t* map)
+{
+    for (int i=0; i<map->cell_count; i++) {
+        map->cells[i].used = 0;
+    }
 }
